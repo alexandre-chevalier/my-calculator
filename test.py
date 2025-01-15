8e87b67ccce0b2f2e1e536ea9cb4e5b9832b04e1
alexandre <alexandre.chevalier@laplateforme.io>
2025-01-15 10:31:23 +0100

    test with external files json/txt with an implemantation later on the main file

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
+"""
 history = ["apple"]
 print(history)
 history.clear()
 print(history)
+"""
+"""
+hello = input("une chaine de caractere : ")
+
+file = open('test.txt', 'a')
+file.write(hello)
+file.close()
+
+file = open('test.txt', 'r')
+for i in file:
+    print(i.strip())
+file.close
+
+"""
+
+import json
+
+number1= int("input")
